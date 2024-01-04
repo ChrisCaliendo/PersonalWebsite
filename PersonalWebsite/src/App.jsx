8b0a7b2e25e1dd32ccbaeb488e780c1e163dbd73
ChrisCaliendo <90165610+ChrisCaliendo@users.noreply.github.com>
2024-01-04 18:53:49 -0500

    more stuff

--- PersonalWebsite/src/App.jsx
+++ PersonalWebsite/src/App.jsx
@@ -9,6 +9,8 @@ import WebApps from './Pages/WebApps';
 import Games from './Pages/Games';
 import Software from './Pages/Software';
 import Experience from './Pages/Experience';
+import Contact from './Pages/Contact';
+import Projects from './Pages/Projects';
 
 function App() {
   return (
@@ -18,12 +20,11 @@ function App() {
           <Header />
             <div>
               <Routes>
-                <Route exact path="/" element={<Home/>} />
-                <Route path="skills" element={<Skills/>}/>
-                <Route path="web_apps" element={<WebApps/>}/>
-                <Route path="games" element={<Games/>}/>
-                <Route path="software" element={<Software/>}/>
-                <Route path="experience" element={<Experience/>}/>
+                <Route exact path="/" element={<Home/>}/>
+                <Route path="/skills" element={<Skills/>}/>
+                <Route path="/projects" element={<Projects/>}/>
+                <Route path="/experience" element={<Experience/>}/>
+                <Route path="/contact" element={<Contact/>}/>
               </Routes>
             </div>
           
--- PersonalWebsite/src/Components/Header.jsx
+++ PersonalWebsite/src/Components/Header.jsx
@@ -1,22 +1,58 @@
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
+
 const Header = () => {
+
+    const [isMobile, setIsMobile] = useState(false);
+    
+    useEffect(() => {
+        const handleResize = () => {
+          setIsMobile(window.innerWidth <= 770); // Adjust the threshold as needed
+        };
+        handleResize();
+        window.addEventListener('resize', handleResize);
+        return () => {
+        window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+        
     return (  
         <>
         <div className="text-white bg-slate-800">
         <nav className="bg-emerald-600 p-4">
-        <div className="container mx-auto flex items-center justify-between">
+        {isMobile ? 
+            <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+            <div className="text-white text-xl font-bold">Christian Caliendo</div>
+            <button data-collapse-toggle="navbar-hamburger" type="button" class="inline-flex items-center justify-center p-2 w-10 h-10 text-sm text-white rounded-lg hover:bg-emerald-800 focus:outline-none focus:ring-2 focus:ring-gray-100 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-hamburger" aria-expanded="false">
+                <span class="sr-only">Open main menu</span>
+                <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
+                </svg>
+            </button>
+            <div class="hidden w-full" id="navbar-hamburger">
+                <ul class="flex flex-col font-medium mt-4 rounded-lg bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
+                    <li><Link className="block py-2 px-3 text-white bg-blue-700 rounded dark:bg-blue-600" to="/home">Home</Link></li>
+                    <li><Link className="block py-2 px-3 text-white bg-blue-700 rounded dark:bg-blue-600" to="/skills">Skills</Link></li>
+                    <li><Link className="block py-2 px-3 text-white bg-blue-700 rounded dark:bg-blue-600" to="/experience">Experience</Link></li>
+                    <li><Link className="block py-2 px-3 text-white bg-blue-700 rounded dark:bg-blue-600" to="/projects">Projects</Link></li>
+                    <li><Link className="block py-2 px-3 text-white bg-blue-700 rounded dark:bg-blue-600" to="/contact">Contact</Link></li>
+                </ul>
+            </div>
+            </div>
+        :
+            <div className="container mx-auto flex items-center justify-between">
             <div className="text-white text-xl font-bold">Christian Caliendo</div>
             <ul className="flex space-x-4">
-            <li><Link className="navbar-option" to="/home">Home</Link></li>
-            <li><Link className="text-white text-xl font-bold" to="/skills">Skills</Link></li>
-            <li><Link className="text-white text-xl font-bold" to="/experience">Experience</Link></li>
-            <li><Link className="text-white text-xl font-bold" to="/web_apps">Web Apps</Link></li>
-            <li><Link className="text-white text-xl font-bold" to="/software">Software</Link></li>
-            <li><Link className="text-white text-xl font-bold" to="/games">Games</Link></li>
-            <li><Link className="text-white text-xl font-bold" to="/contact">Contact</Link></li>
+                <li><Link className="text-white text-xl font-bold hover:text-slate-300 hover:bg-emerald-800" to="/home">Home</Link></li>
+                <li><Link className="text-white text-xl font-bold" to="/skills">Skills</Link></li>
+                <li><Link className="text-white text-xl font-bold" to="/experience">Experience</Link></li>
+                <li><Link className="text-white text-xl font-bold" to="/projects">Projects</Link></li>   
+                <li><Link className="text-white text-xl font-bold" to="/contact">Contact</Link></li>
             </ul>
-        </div>
+            </div>
+        }
+        
         </nav>
         </div>
         </>

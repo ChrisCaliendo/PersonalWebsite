208b0e43b410a1408b925b85a58d5a496588959e
ChrisCaliendo <90165610+ChrisCaliendo@users.noreply.github.com>
2024-01-08 11:58:47 -0500

    Update Projects.jsx

--- PersonalWebsite/src/Pages/Projects.jsx
+++ PersonalWebsite/src/Pages/Projects.jsx
@@ -7,44 +7,85 @@ const Projects = () => {
             <div className="p-3  bg-purple-300 rounded-md shadow-md">
                 <div className=" grid grid-cols-2 grid-rows-1">
                     <div>
-                        <p>Software Engineer</p>
-                        <p>Career Advantage Inc</p>
+                        <p>Eloquent Reviews</p>
+                        <p className="italic">JavaScript, React.js, Tailwind, Python, Flask</p>
                     </div>
                     <img className="w-20 rounded-2xl" src="https://media.licdn.com/dms/image/C4E0BAQF6OYSlqipOWQ/company-logo_200_200/0/1631329033032?e=1712793600&v=beta&t=R8S98_o1iY2opiVjjrW-mU0zX0inNv2rQZV8B9KpDjo" alt="" />
                 </div>
                 
                 <ul className="ps-5 mt-2 space-y-1 list-disc list-inside">
-                    <li>Remade and reimagined company site using React and TailwindCSS</li>
-                    <li>Hosted site using AWS: S3 and Route 53</li>
-                    <li>Engineered a dynamic webpage for job listing page to render and display hundreds of current job openings in the company based on job listing data</li>
-                    <li>Designed and made all pages and components with responsive web design</li>
+                    <li>Performed as Sole Engineer of a dynamic web application to find and display product reviews</li>
+                    <li>Built a frontend with a responsive form and user interface to display reviews and alter preferences</li>
+                    <li>Implemented back-end and web scraping capabilities with Python, Flask and BeautifulSoup to scrape from thousands of reviews from Steam</li>
+                    <li>Used the Steam API for text processing and sorting based on user preferences</li>
                 </ul>
+                <p>GitHub Code: https://github.com/ChrisCaliendo/EloquentReviews</p>
             </div>
             <div className="p-3 bg-purple-300 rounded-md shadow-md">
                 
                 <div className="grid grid-cols-2 grid-rows-1 place-content-center">
                     <div>
-                        <p>Software Engineer</p>
-                        <p>Gaming and Emerging Technologies Club</p>
+                        <p>Cost Minus Drugs</p>
+                        <p className="italic">MongoDB, Express, React.js, Node.js, ChakraUI</p>
                     </div>
                 </div>
                 
                 
                 <ul className="ps-5 mt-2 space-y-1 list-disc list-inside">
-                    <li> Contributed to 5 major group projects involving programming with C# and Unity Game Engine to develop games</li>
-                    <li> Created and finished 2 award-winning and publicly accessible games with collaboration from many participants with wide variety of skill sets</li>
-                    <li>  Managed team to develop AR technology using Unreal Engine and C++</li>
+                    <li>Developed a prototype full-stack e-commerce website that distributes and sells generic drugs for affordable pricing, all with 24 hours</li>
+                    <li>Served as the lead frontend engineer, leveraging React and ChakraUI</li>
+                    <li>Won beginner prize at Divhacks hackathon 2023</li>
                 </ul>
+                <p> GitHub Code: https://github.com/Sajid2001/divhacks-project</p>
+            </div>
+            <div className="p-3 bg-purple-300 rounded-md shadow-md">
+                
+                <div className="grid grid-cols-2 grid-rows-1 place-content-center">
+                    <div>
+                        <p>Omniswipe</p>
+                        <p className="italic">React Native, Expo, Firebase Firestore, Git</p>
+                    </div>
+                </div>
+                
+                
+                <ul className="ps-5 mt-2 space-y-1 list-disc list-inside">
+                    <li>Contributed to 5 major group projects involving programming with C# and Unity Game Engine to develop games</li>
+                    <li>Created and finished 2 award-winning and publicly accessible games with collaboration from many participants with wide variety of skill sets</li>
+                    <li>Managed team to develop AR technology using Unreal Engine and C++</li>
+                </ul>
+                <p>GitHub Code: https://github.com/JuanPringle/OmniSwipe</p>
+            </div>
+            <div className="p-3 bg-purple-300 rounded-md shadow-md">
+                
+                <div className="grid grid-cols-2 grid-rows-1 place-content-center">
+                    <div>
+                        <p>Vehicle Cloud Management Software</p>
+                        <p className="italic">Java, JFrame, MySQL, Git</p>
+                    </div>
+                </div>
+                
+                
+                <ul className="ps-5 mt-2 space-y-1 list-disc list-inside">
+                    <li>Served as lead programmer in the development of a Vehicular cloud program, alongside 3 other developers, using Java and JFrame</li>
+                    <li>Functioned as lead programmer of backend and engineer for data management database using Java and MySQL</li>
+                    <li>Connected frontend interface to backend data storage with multithreading.</li>
+                </ul>
+                <p>GitHub Code: https://github.com/jlin7500/GUI</p>
             </div>
             <mark class="text-center md:col-span-1 lg:col-span-2 text-4xl font-bold ppx-2 text-white bg-blue-600 rounded dark:bg-blue-500">Games</mark>
             <div className="pl-3 bg-blue-300 rounded-md shadow-md">
-                <p>Bachelors Degree, in Computer Science</p>
-                <p>St. Johns University</p>
+                <p>RiGBY</p>
+                <p></p>
                 
             </div>
             <div className="pl-3 bg-blue-300 rounded-md shadow-md">
-                <p>High School Degree</p>
-                <p>Queens Highschool of Teaching</p>
+                <p>Walbang!</p>
+                <p></p>
+                
+            </div>
+            <div className="pl-3 bg-blue-300 rounded-md shadow-md">
+                <p>Trash Job</p>
+                <p></p>
                 
             </div>
         </div>

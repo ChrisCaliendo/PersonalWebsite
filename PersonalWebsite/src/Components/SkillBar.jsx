1b0ac6ad5bedb96e14e1b704b0e6f29047fbed41
ChrisCaliendo <90165610+ChrisCaliendo@users.noreply.github.com>
2024-01-15 13:26:29 -0500

    added a bit to skills

--- PersonalWebsite/src/Components/SkillBar.jsx
+++ PersonalWebsite/src/Components/SkillBar.jsx
@@ -1,4 +1,17 @@
 const SkillBar = ({name, progress}) => {
+    let level = "In Progress";
+    if (progress<=20) {
+        level = "Beginner";
+    } else if (progress<=40) {
+        level = "Intermediate";
+    } else if (progress<=60) {
+        level = "Proficient";
+    } else if (progress<=80) {
+        level = "Exceptional";
+    } else {
+        level = "Expert";
+    }
+
     return (  
         <div className="px-5 pb-4 ">
             <div className="bg-white bg-opacity-50 rounded-t-lg">
@@ -6,12 +19,13 @@ const SkillBar = ({name, progress}) => {
                     {name} 
                 </p>
             </div>
-            <div className="bg-slate-400 bg-opacity-50 rounded-b-lg">
+            <div className="bg-slate-400 bg-opacity-50 rounded-b-lg font-bold">
                 <div 
-                    className="opacity-100 p-2 bg-green-700 text-xs font-medium text-blue-100 text-center leading-none rounded-b-lg "
+                    className="opacity-100 p-1 bg-green-700 text-xs font-medium text-blue-100 text-left leading-none rounded-b-lg "
                     style={{width: `${progress}%`}}
                 > 
-                    {}
+                <p className="pl-3 opacity-100">{level}</p>
+                    
                 </div> 
             </div>
         </div>
--- PersonalWebsite/src/Pages/Skills.jsx
+++ PersonalWebsite/src/Pages/Skills.jsx
@@ -6,7 +6,7 @@ const Skills = () => {
         <p className="text-4xl bg-emerald-500 flex justify-center font-ptSans font-bold">Skills</p>
         <div className="bg-emerald-300 grid grid-rows-3 grid-cols-2 gap-4 p-4 place-content-evenly font-ptSans font-bold">
             <div className="bg-purple-400 rounded-md shadow-md col-span-2 md:col-span-1">
-                <p className="text-2xl text-center">Coding Languages</p>
+                <p className="text-2xl p-1 text-center">Coding Languages</p>
                 <SkillBar name="Java" progress={100}/>
                 <SkillBar name="JavaScript" progress={100}/>
                 <SkillBar name="Python" progress={80}/>
@@ -15,7 +15,7 @@ const Skills = () => {
                 <SkillBar name="C++" progress={20}/>
             </div>
             <div className="bg-orange-300 rounded-md shadow-md col-span-2 md:col-span-1">
-                <p className="text-2xl text-center">Frameworks</p>
+                <p className="text-2xl p-1 text-center">Frameworks</p>
                 <SkillBar name="React" progress={100}/>
                 <SkillBar name="Node.js" progress={80}/>
                 <SkillBar name="Flask" progress={60}/>
@@ -24,7 +24,7 @@ const Skills = () => {
                 <SkillBar name="Angular" progress={20}/>
             </div>
             <div className=" bg-cyan-200 rounded-md shadow-md col-span-2 md:col-span-1">
-                <p className="text-2xl text-center">Methodologies</p>
+                <p className="text-2xl p-1 text-center">Methodologies</p>
                 <SkillBar name="Object-Oriented-Programming" progress={100}/>
                 <SkillBar name="Agile Development" progress={60}/>
                 <SkillBar name="DevOps" progress={40}/>
@@ -33,7 +33,7 @@ const Skills = () => {
                 <SkillBar name="Automated Testing" progress={40}/>
             </div>
             <div className=" bg-amber-200 rounded-md shadow-md col-span-2 md:col-span-1">
-                <p className="text-2xl text-center">Developer Tools</p>
+                <p className="text-2xl p-1 text-center">Developer Tools</p>
                 <SkillBar name="Amazon Web Services" progress={60}/>
                 <SkillBar name="MongoDB" progress={60}/>
                 <SkillBar name="Firebase" progress={60}/>

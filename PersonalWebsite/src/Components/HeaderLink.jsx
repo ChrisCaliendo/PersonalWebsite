8b2032898cb6b3dc15fae1db93f96b1de71cd7f9
ChrisCaliendo <90165610+ChrisCaliendo@users.noreply.github.com>
2024-01-30 16:42:32 -0500

    Added cool header effect with useLocation

--- PersonalWebsite/src/Components/HeaderLink.jsx
+++ PersonalWebsite/src/Components/HeaderLink.jsx
@@ -1,9 +1,13 @@
 import { Link, useLocation } from "react-router-dom";
 const HeaderLink = (props) => {
     const location = useLocation();
+    const isActive = (path) => {
+        return location.pathname === path ? 'active' : '';
+    };
+
     return (  
         <li>
-            <Link className="disabled:text-blue-500 p-1 pb-2 px-2 rounded-xl transition ease-in-out duration-150 text-white text-xl font-bold hover:text-slate-300 hover:bg-emerald-800" disabled={location.pathname === props.pathway} to={props.pathway}>{props.name}</Link>
+            <Link className={`${location.pathname === props.pathway ? "text-emerald-800 bg-white" : "text-white hover:text-slate-300 hover:bg-emerald-800"} p-1 pb-2 px-2 rounded-xl transition ease-in-out duration-150  text-xl font-bold `} to={props.pathway}>{props.name}</Link>
         </li>
     );
 }

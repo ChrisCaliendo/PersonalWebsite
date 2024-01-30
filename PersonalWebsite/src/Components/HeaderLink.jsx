e46f8e43faa2498d3dc823b49b397963f6c5161d
ChrisCaliendo <90165610+ChrisCaliendo@users.noreply.github.com>
2024-01-30 13:04:49 -0500

    Update HeaderLink.jsx

--- PersonalWebsite/src/Components/HeaderLink.jsx
+++ PersonalWebsite/src/Components/HeaderLink.jsx
@@ -3,7 +3,7 @@ const HeaderLink = (props) => {
     const location = useLocation();
     return (  
         <li>
-            <Link className="p-1 pb-2 px-2 rounded-xl transition ease-in-out duration-150 text-white text-xl font-bold hover:text-slate-300 hover:bg-emerald-800" disabled={location.pathname == props.pathway} to={props.pathway}>{props.name}</Link>
+            <Link className="disabled:text-blue-500 p-1 pb-2 px-2 rounded-xl transition ease-in-out duration-150 text-white text-xl font-bold hover:text-slate-300 hover:bg-emerald-800" disabled={location.pathname === props.pathway} to={props.pathway}>{props.name}</Link>
         </li>
     );
 }

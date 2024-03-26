ccd24befeb0c938fb9d3d0a3a93e849ce53c2ec5
Christian Caliendo <90165610+ChrisCaliendo@users.noreply.github.com>
2024-03-26 18:14:46 -0400

    Update ExpCard.jsx

--- PersonalWebsite/src/Components/CardComponents/ExpCard.jsx
+++ PersonalWebsite/src/Components/CardComponents/ExpCard.jsx
@@ -1,8 +1,8 @@
 const ExpCard = ({title, desc, photoUrl, employer}) => {
     return ( 
-        <div className=" p-2 grid grid-cols-1 grid-rows-1 place-content-center h-88">
+        <div className=" p-2 grid grid-cols-1 grid-rows-1 gap-0 place-content-center h-full">
             <div>
-            <div className={`flex px-3 pr-6 py-2 border-slate-800 bg-slate-200 col-span-1 grow justify-between rounded-t-2xl`}>
+            <div className={`flex px-3 pr-6 py-2 border-slate-800 bg-slate-200 col-span-1 grow justify-between rounded-t-2xl h-full`}>
                 
                 <div>
                     <p className="text-2xl">{title}</p>
@@ -10,7 +10,7 @@ const ExpCard = ({title, desc, photoUrl, employer}) => {
                 </div>
                 {
                 (photoUrl != "" && photoUrl != null) &&
-                <div className="flex w-40 xl:w-40 2xl:w-30">
+                <div className="flex w-40 xl:w-40 2xl:w-30 h-fill">
                     <img className="object-fill m-2 rounded-2xl border-8 border-slate-800" src={photoUrl} alt="No Photo Available :(" />
                 </div>
                 }
@@ -19,7 +19,7 @@ const ExpCard = ({title, desc, photoUrl, employer}) => {
             </div>
             
             
-            <div className="row-end px-3 py-2 rounded-b-2xl border-t-4 border-slate-800 col-span-2 bg-white bg-opacity-50 h-60">
+            <div className="grow row-end px-3 py-2 rounded-b-2xl border-t-4 border-slate-800 col-span-2 bg-white bg-opacity-50 h-full">
                 <p className="">{desc}</p>
             </div>
             

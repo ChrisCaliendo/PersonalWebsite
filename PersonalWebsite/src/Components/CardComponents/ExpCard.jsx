3fff40be40a247ef0772226fbd41782cc7580b37
Christian Caliendo <90165610+ChrisCaliendo@users.noreply.github.com>
2024-06-20 13:24:05 -0400

    Update ExpCard.jsx

--- PersonalWebsite/src/Components/CardComponents/ExpCard.jsx
+++ PersonalWebsite/src/Components/CardComponents/ExpCard.jsx
@@ -2,7 +2,7 @@ const ExpCard = ({title, desc, photoUrl, employer}) => {
     return ( 
         <div className=" p-2 grid grid-cols-1 grid-rows-1 gap-0 place-content-center h-full">
             <div className="flex flex-col">
-                <div className={`flex px-3 pr-6 py-2 border-slate-800 bg-slate-200 col-span-1  justify-between rounded-t-2xl `}>
+                <div className={`flex px-3 pr-6 py-2 border-slate-800 bg-slate-200 col-span-1 h-fit justify-between rounded-t-2xl `}>
                     
                     <div>
                         <p className="text-2xl">{title}</p>
@@ -10,7 +10,7 @@ const ExpCard = ({title, desc, photoUrl, employer}) => {
                     </div>
                     {
                     (photoUrl != "" && photoUrl != null) &&
-                    <div className="flex  aspect-square max-w-24 xs:max-w-32 xs:min-w-32 sm:max-w-40 sm:min-w-40">
+                    <div className="flex aspect-[1/1] max-w-24 xs:max-w-32 xs:min-w-32 sm:max-w-40 sm:min-w-40">
                         <img className="object-fill m-2 rounded-2xl border-8 border-slate-800" src={photoUrl} alt="No Photo Available :(" />
                     </div>
                     }
